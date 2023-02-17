b0612f03cb3320402afdc0224879c86d471f2a31
Abdellatif Al Kerde <boudikerde0@gmail.com>
2023-02-17 22:22:35 +0200

    step3

---
+++ php-basics-01/step3.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Abdellatif";
+$email = "boudikerde0@gmail.com";
+$height = 100;
+$width = 50;
+$area = $height * $width;
+echo "My name is $name and my email is $email \n";
+echo "My area value is $area \n"
+// echo "The area value is: "$height * $width;
+?>
