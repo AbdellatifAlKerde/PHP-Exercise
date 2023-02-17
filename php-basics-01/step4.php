93917319e1d5ca9dec23498d0498d1768e949d9b
Abdellatif Al Kerde <boudikerde0@gmail.com>
2023-02-17 22:37:57 +0200

    step4

---
+++ php-basics-01/step4.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Abdellatif";
+$position = "Full stack web developer";
+$github_url = "https://github.com/AbdellatifAlKerde/PHP-Exercise";
+
+echo "Hello, I'm $name, I'm a $position please check my github link $github_url \n";
+echo "Hello, I'm ".$name.", I'm a ".$position." please check my Github link: ".$github_url."\n";
+
+?>
