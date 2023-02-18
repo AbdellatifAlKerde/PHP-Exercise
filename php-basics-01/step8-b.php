4acd304f870d7da902485fc70ef0350a7a8ab48b
Abdellatif Al Kerde <boudikerde0@gmail.com>
2023-02-18 14:55:45 +0200

    step8 - b

--- php-basics-01/step8-b.php
+++ php-basics-01/step8-b.php
@@ -4,5 +4,8 @@ $array = [
   "milk" => ["Fresh", "Taanayel"],
   "water-pack" => ["Tanoureen", "Reem"]
 ];
+$groceries = array("Eggs", "Milk", "Cheese", "Water", "Pack", "Tissues", "Watermelon");
 
 echo "Hey Sir, Please I need 1 pack of {$array['eggs'][0]} eggs and 3 {$array['water-pack'][1]} Water Pack.\n";
+echo "Hello Sir, do you have ".$groceries[0].", " .$groceries[1].", and ".$groceries[2]."? Also if you sell fruits can I find a ".$groceries[6]."?\n";
+
