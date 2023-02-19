0c47e41b2ba6e92f7321a4cf9cb3f5515b3159ab
Abdellatif Al Kerde <boudikerde0@gmail.com>
2023-02-19 12:18:05 +0200

    ex3(solution-2)

--- php-basics-03/ex3.php
+++ php-basics-03/ex3.php
@@ -1,11 +1,21 @@
 <?php
 
-function countWords($s, $w) {
-  // $sArray = explode(" ", $s);
+// function countWords($s, $w) {
 
-  echo substr_count($s, "$w")."\n";
+//   echo substr_count($s, "$w")."\n";
+// }
+
+// $input = readline("Write a phrase: ");
+// $word = readline("Enter the word you want to count: ");
+// countWords($input, $word);
+
+function countWords ($s, $w) {
+  $sArray = explode(" ", $s);
+  $sCount = array_count_values($sArray);
+  echo $sCount[$w]."\n";
+    
 }
 
-$input = readline("Write a phrase: ");
-$word = readline("Enter the word you want to count: ");
-countWords($input, $word);
+$s = readline("Write a phrase: ");
+$w = readline("Write a word you want to count: ");
+countWords($s, $w);
