43f769a2b96c7068cd20b02418fae6cebddaf2ea
Abdellatif Al Kerde <boudikerde0@gmail.com>
2023-02-19 12:26:38 +0200

    ex3 (using while loop)

--- php-basics-03/ex3.php
+++ php-basics-03/ex3.php
@@ -9,13 +9,30 @@
 // $word = readline("Enter the word you want to count: ");
 // countWords($input, $word);
 
-function countWords ($s, $w) {
-  $sArray = explode(" ", $s);
-  $sCount = array_count_values($sArray);
-  echo $sCount[$w]."\n";
+// function countWords ($s, $w) {
+//   $sArray = explode(" ", $s);
+//   $sCount = array_count_values($sArray);
+//   echo $sCount[$w]."\n";
     
+// }
+
+// $s = readline("Write a phrase: ");
+// $w = readline("Write a word you want to count: ");
+// countWords($s, $w);
+
+function countWords ($s, $w) {
+  $array = explode(" ", $s);
+  $words = count($array);
+  $i = 0;
+  while ($i < $words) {
+    if ($array[$i] == "$w") {
+      $i += 1;
+    }
+  }
+  echo "$i\n";
 }
 
-$s = readline("Write a phrase: ");
-$w = readline("Write a word you want to count: ");
+$s = readline("Write a sentence: ");
+$w = readline("Write a word: ");
+
 countWords($s, $w);
